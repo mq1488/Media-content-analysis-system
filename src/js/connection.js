ec171355bf5e0531f92bc7d1c91cbc5a6a75f55c
mq1488 <54840317+mq1488@users.noreply.github.com>
2020-12-20 23:02:40 +0200

    Update connection.js

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,13 +1,3 @@
-const Sequilize = require('sequelize');
-const dbConfig = require('../../config/database');
+const Sequelize = require("sequelize");
 
-module.exports = new Sequilize(
-    dbConfig.DATABASE_NAME,
-    dbConfig.DATABASE_USERNAME,
-    dbConfig.DATABASE_PASSWORD,
-    {
-        host: dbConfig.DATABASE_HOST,
-        dialect: dbConfig.DATABASE_DRIVER,
-        logging: false
-    }
-);
+module.exports = new Sequelize(require("../../config/database"));
